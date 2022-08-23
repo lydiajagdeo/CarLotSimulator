a7d742142004a4356c8448aeabda1a69b6fb57f6
Lydia Jagdeo <99758270+lydiajagdeo@users.noreply.github.com>
2022-08-22 20:11:23 -0400

    Create Car.cs

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,54 @@
+using System;
+namespace CarLotSimulator
+{
+    public class Car
+    {
+        public Car()
+        {
+
+        }
+
+
+        public Car(int year,
+            string make, 
+            string model,
+            string engineNoise,
+            string honkNoise,
+            bool isDrivable)
+        {
+            Year = year;
+            Make = make;
+            Model = model;
+            EngineNoise = engineNoise;
+            HonkNoise = honkNoise;
+            IsDriveable = isDrivable;
+
+
+        }
+
+
+        public int Year { get; set; }
+
+        public string? Make { get; set; }
+
+        public string? Model { get; set; }
+
+        public string? EngineNoise { get; set; }
+
+        public string? HonkNoise { get; set; }
+
+        public bool IsDriveable { get; set; }
+
+
+       public void MakeEngineNoise(string engineNoise)
+        {
+            Console.WriteLine($" This is what my car's engine sounds like {EngineNoise}");
+        }
+
+        public void MakeHonkNoise()
+        {
+            Console.WriteLine($" This is what my car's honk sounds like {HonkNoise}");
+        }
+    }
+}
+
