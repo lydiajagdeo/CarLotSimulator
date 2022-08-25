bb210710537b7125bf99257c23462b6cb5b704df
Lydia Jagdeo <99758270+lydiajagdeo@users.noreply.github.com>
2022-08-24 21:05:10 -0400

    Create CarLot.cs

---
+++ CarLot.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+
+namespace CarLotSimulator
+{
+    public class CarLot
+    {
+        public CarLot()
+        {
+        }
+
+        public static int numberOfCars = 1;
+
+
+
+
+        public List<Car> Cars = new List<Car>();
+    }
+}
